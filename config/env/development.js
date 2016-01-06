d7e51f2ebc9528aea24f5d11ed379baeb83e4490
Veijo Väisänen <veijo.vaisanen@oamk.fi>
2016-01-06 18:48:52 +0200

    Database changed in env/development.js

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,7 @@
 module.exports = {
 sessionSecret: 'developmentSessionSecret',
-db: 'mongodb://localhost/mean-book',
+//db: 'mongodb://localhost/mean-book',
+db: 'mongodb://vvaisan:Miisu#Kiti@waffle.modulusmongo.net:27017/etiRyw3y',
 facebook: {
 clientID: '887361671377693',
 clientSecret: '96c45b65bdc7ea9d7b9fc5e5eb940905',

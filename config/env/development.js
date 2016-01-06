6c01f1df170656369f4a2e34f6266fbe51ca9000
Veijo Väisänen <veijo.vaisanen@oamk.fi>
2016-01-06 17:46:36 +0200

    First release

---
+++ config/env/development.js
@@ -0,0 +1,10 @@
+module.exports = {
+sessionSecret: 'developmentSessionSecret',
+db: 'mongodb://localhost/mean-book',
+facebook: {
+clientID: '887361671377693',
+clientSecret: '96c45b65bdc7ea9d7b9fc5e5eb940905',
+callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+}
+};
+ 
